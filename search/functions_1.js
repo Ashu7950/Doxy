7c64a982cf326d309bee3e8c9600206d8de798d5
Ashu7950 <Ashu7950@users.noreply.github.com>
2024-03-07 09:53:41 +0000

    deploy: 108d8bf9c9168e4ac0e0a7aea274309234bdc6f1

--- search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['testme_0',['testMe',['../classQTstyle__Test.html#a8840748753118dd468e8368a28e49c62',1,'QTstyle_Test']]],
-  ['testmetoo_1',['testMeToo',['../classQTstyle__Test.html#ad5b201f097a720d44bf976c2f27efbda',1,'QTstyle_Test']]]
-];
